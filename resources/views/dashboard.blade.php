56025f695eea9538f24f7f15ab10d167d539221a
Lluc <llucnf@gmail.com>
2022-03-22 16:42:07 +0100

    cambio h2 en dashboard y routserviceprovider ruta home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,14 +2,14 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            esto es la ramaaaaaa
         </h2>
     </x-slot>
 
     <div class="py-12">
         
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                @yield('tablaPlatos')
+              
             </div>
         
     </div>
