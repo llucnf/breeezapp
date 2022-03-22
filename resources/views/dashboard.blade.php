83552a98e337c4bc0819cebc942a8fed39d10566
Lluc <llucnf@gmail.com>
2022-03-22 16:53:05 +0100

    add h2 en dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,14 +2,16 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            esto es la ramaaaaaa
+            dashboard
         </h2>
     </x-slot>
 
     <div class="py-12">
         
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-              
+              <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            esto es de la ramaaaaaa 1
+        </h2>
             </div>
         
     </div>
